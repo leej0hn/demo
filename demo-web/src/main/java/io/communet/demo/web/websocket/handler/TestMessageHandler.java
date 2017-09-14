41ede3eb1533bf7a630cad347a03b9e64b0930f0
LeeJohn <lidx@communet.io>
2017-09-14 16:13:23 +0800

    调整websocket1

--- demo-web/src/main/java/io/communet/demo/web/websocket/handler/TestMessageHandler.java
+++ demo-web/src/main/java/io/communet/demo/web/websocket/handler/TestMessageHandler.java
@@ -1,9 +1,10 @@
 package io.communet.demo.web.websocket.handler;
 
 import io.communet.demo.service.TestService;
-import io.communet.demo.web.component.SpringContextHolder;
 import io.communet.demo.web.utils.WebsocketUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.websocket.MessageHandler;
 
@@ -14,8 +15,10 @@ import javax.websocket.MessageHandler;
  * <p>Version: 1.0
  */
 @Slf4j
+@Component
 public class TestMessageHandler implements MessageHandler.Whole<String> {
-    private TestService testService = (TestService) SpringContextHolder.getBean("testService");
+    @Autowired
+    private TestService testService ;
 
     @Override
     public void onMessage(String message) {
