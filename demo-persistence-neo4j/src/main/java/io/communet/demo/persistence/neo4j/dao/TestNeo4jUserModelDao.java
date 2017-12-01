826cf9ebe5f92f1708387fde0875c72a4738618b
LeeJohn <lidx@communet.io>
2017-12-01 13:06:27 +0800

    neo4j @query语句 demo

--- demo-persistence-neo4j/src/main/java/io/communet/demo/persistence/neo4j/dao/TestNeo4jUserModelDao.java
+++ demo-persistence-neo4j/src/main/java/io/communet/demo/persistence/neo4j/dao/TestNeo4jUserModelDao.java
@@ -3,6 +3,9 @@ package io.communet.demo.persistence.neo4j.dao;
 
 import io.communet.demo.common.model.neo4j.TestUserModel;
 import io.communet.demo.persistence.neo4j.repository.CustomerNeo4jRepository;
+import org.springframework.data.neo4j.annotation.Query;
+
+import java.util.List;
 
 /**
  * <p>function:
@@ -11,5 +14,18 @@ import io.communet.demo.persistence.neo4j.repository.CustomerNeo4jRepository;
  * <p>Version: 1.0
  */
 public interface TestNeo4jUserModelDao extends CustomerNeo4jRepository<TestUserModel, Long> {
+    /* 其他案例语句
+    //通过订单ID查询
+    @Query("MATCH (taobao_order_list{order_id:{0}}) <- [r] - (n:mobile_pegging_wechat) WITH n MATCH p = (n) - [*0..1] - (m) RETURN p")
+    MobilePeggingWechat findBytaobaoOrderListId(String orderId);
 
+    //综合查询，订单ID，mobile, nickname
+    @Query("MATCH (a:taobao_order_list) <- [r] - (n:mobile_pegging_wechat) WHERE a.order_id =~ {0} AND n.mobile =~ {1} AND n.nickname =~ {2} WITH n MATCH p = (n) - [*0..1] - (m) RETURN p")
+    List<MobilePeggingWechat> multipleFind(String orderId , String mobile, String nickname);
+
+    //通过授权粉丝 appidOpenid查询
+    @Query("MATCH (a:ccrm_auth_fans) <- [r] - (n:mobile_pegging_wechat) WHERE a.appid_openid = {0}  WITH n MATCH p = (n) - [*0..1] - (m) RETURN p")
+    MobilePeggingWechat findByAuthFansAppidOpenid(String appidOpenid);
+
+    */
 }
