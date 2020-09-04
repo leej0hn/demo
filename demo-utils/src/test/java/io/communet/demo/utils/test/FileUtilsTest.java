ed0e59da53294b0ce3a06f850bc8eb0581e6cac5
lidongxiao01 <lidongxiao01@countrygarden.com.cn>
2020-09-04 15:50:11 +0800

    update : 增加 FileInputStream 与 FileOutputStream使用区别

--- demo-utils/src/test/java/io/communet/demo/utils/test/FileUtilsTest.java
+++ demo-utils/src/test/java/io/communet/demo/utils/test/FileUtilsTest.java
@@ -5,6 +5,8 @@ import io.communet.demo.utils.JsonFileUtils;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 /**
  * <p>function:
@@ -14,14 +16,43 @@ import java.io.File;
  */
 public class FileUtilsTest {
     @Test
-    public void testFileWriteString(){
-        FileUtils.writeString("asdfadf",new File("C:\\Users\\LeeJohn\\Documents\\雷电模拟器\\Misc\\test.txt"));
+    public void testFileWriteString() {
+        FileUtils.writeString("asdfadf", new File("C:\\Users\\LeeJohn\\Documents\\雷电模拟器\\Misc\\test.txt"));
     }
 
     @Test
-    public void testJsonWriteString(){
+    public void testJsonWriteString() {
         String path = "C:\\Users\\LeeJohn\\Documents\\雷电模拟器\\Misc";
         String filePath = "C:\\Users\\LeeJohn\\Documents\\雷电模拟器\\Misc\\test.txt";
-        JsonFileUtils.put(path,filePath,"a","123");
+        JsonFileUtils.put(path, filePath, "a", "123");
+    }
+
+    @Test
+    public static void main(String[] arg) {
+        File f = new File("1.txt");
+        //向文件里写如"Hello"字符串.
+        try {
+            //要写入的数据转换成字节数组
+            byte[] buf = "Hello".getBytes();
+            //如果1.txt存在,则删除1.txt里面的内容,文本所有内容变为Hello
+            //如果1.txt不存在,在新建1.txt文本,写入Hello
+            FileOutputStream out = new FileOutputStream(f);
+            out.write(buf);
+            out.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        //读取文件中的内容。可在程序中单独使用,不用关心"写"是否存在.
+        try {
+            //只要f存在就可以读出f的内容,与写操作代码没有关联性.
+            FileInputStream in = new FileInputStream(f);
+            byte[] buf = new byte[1024];
+            int len = in.read(buf);        //从流中读取内容
+            String str = new String(buf, 0, len);
+            System.out.println(str);    //打印f文件的内容.
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
